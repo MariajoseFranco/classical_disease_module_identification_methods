8a4e3dcdd15390dce367a44d62c7fa2774ee1593
Mariajose Franco Orozco <mariajosefranco@Mariajoses-MacBook-Pro.local>
2025-05-13 11:42:45 +0200

    new domino class functioning

--- classical_methods/domino_algorithm_nuevo.py
+++ classical_methods/domino_algorithm_nuevo.py
@@ -8,7 +8,10 @@ import pandas as pd
 
 # Absolute path to the folder containing the module
 sys.path.append(os.path.abspath('/Users/mariajosefranco/Desktop/Data Science - UPM/TFM/project/state_of_art_repos/DOMINO'))
-from src.core.domino import main as domino_main
+from src.runner import main_domino, main_slicer
+
+# from src.core.domino import main as main_domino
+# from src.core.preprocess_slices import main as main_slicer
 
 
 class DOMINO:
@@ -28,12 +31,41 @@ class DOMINO:
                 seeds_file.write(f"{node}\n")
             seeds_path = seeds_file.name
 
-        # Call domino_main with paths
-        domino_main(seeds_path, ppi_path)
+        # Create slices file
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as slices_file:
+            slices_path = slices_file.name
 
-        # Clean up
+        # Create output folder
+        output_folder = tempfile.mkdtemp()
+
+        # Step 1: run slicer
+        sys.argv = [
+            "preprocess_slices.py",
+            "-n", ppi_path,
+            "-o", slices_path
+        ]
+        main_slicer()
+
+        # Step 2: run DOMINO with full args
+        sys.argv = [
+            "runner.py",
+            "-a", seeds_path,
+            "-n", ppi_path,
+            "-s", slices_path,
+            "-o", output_folder,
+            "-c", "false",              # avoid pickle cache
+            "-p", "1",                  # single-thread
+            "-v", "false",              # skip visualization
+            "-sth", "0.3",              # slice threshold
+            "-mth", "0.05"              # module threshold
+        ]
+        G_final_modules = main_domino()
+
+        # Clean up if needed:
         os.remove(ppi_path)
         os.remove(seeds_path)
+        os.remove(slices_path)
+        return G_final_modules
 
     def run_domino(self, ppi_path: str, seed_nodes_path: str):
         # Step 1: Load inputs
@@ -54,4 +86,5 @@ class DOMINO:
         G = nx.from_pandas_edgelist(ppi_df, source="protein1", target="protein2")
 
         # Run the DOMINO pipeline
-        self._run_domino_pipeline(G, seed_nodes)
+        G_final_modules = self._run_domino_pipeline(G, seed_nodes)
+        return G_final_modules
